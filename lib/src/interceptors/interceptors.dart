23c1d1e4116b304dfb870f852f9484ead738c45b
Hiren Raval <hirenravalwork@gmail.com>
2024-12-10 23:43:32 +0530

    - initial commit - add network package

---
+++ lib/src/interceptors/interceptors.dart
@@ -0,0 +1,5 @@
+import 'package:network/src/token_data.dart';
+
+typedef TokenDataCallback = void Function(TokenData data);
+typedef GetTokenCallback = String? Function();
+typedef GetHeadersCallback = Map<String, dynamic> Function();
